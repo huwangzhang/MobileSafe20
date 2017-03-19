f3e9c1ee65610a2bfb071bfd6e7a80d79dee2005
huwang <windang@163.com>
2017-03-19 15:05:01 +0800

    第二次提交：更新设置

--- app/src/main/java/com/example/huwang/mobilesafe20/activity/SplashActivity.java
+++ app/src/main/java/com/example/huwang/mobilesafe20/activity/SplashActivity.java
@@ -8,11 +8,18 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 
 import com.example.huwang.mobilesafe20.R;
+import com.example.huwang.mobilesafe20.utils.HttpUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by huwang on 2017/3/16.
@@ -37,27 +44,77 @@ public class SplashActivity extends Activity {
         PackageManager pm = getPackageManager();
         // PackageInfo info=pm.getPackageInfo(包名, 标签数据);
         try {
-            PackageInfo info = pm.getPackageInfo("com.example.huwang.mobilesafe", 0);
-            int localVersionCode = info.versionCode;
+            PackageInfo info = pm.getPackageInfo("com.example.huwang.mobilesafe20", 0);
+            localVersionCode = info.versionCode;
             String versionName = info.versionName;
             System.out.print(versionName);
             versiontext.setText(versionName);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-         new Thread(){
-             public void run() {
+        //获取版本信息
+        new Thread(){
+            @Override
+            public void run() {
+                //获取数据
                 try {
-                    Thread.sleep(3000);
-                    //意图：激活Activity或者Service的重要对象。
-                    startActivity(new Intent(getBaseContext(), HomeActivity.class));
-                    //关闭
-                    finish();
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
+                    String json = HttpUtil.get("http://192.168.161.226:8080/update70/jsoninfo");
+                    System.out.println(json);
+                    //解析数据
+                    //{versioncode:2,versionName:"黑马福利版2.0",url:"/update70/MobileSafe70.apk",desc:"快快下载，年底可以参与抽奖，奖品：与avXXX见面"}
+                    if (json != null) {
+
+                        JSONObject obj = new JSONObject(json);
+                        int serverversionCode = obj.getInt("versioncode");
+                        String versionName = obj.getString("versionName");
+                        String desc = obj.getString("desc");
+                        String url = obj.getString("url");
+                        if (localVersionCode < serverversionCode) {
+                            //
+                            Message msg = handler.obtainMessage();
+                            msg.what = CODE_UPDATE;
+                            handler.sendMessage(msg);
+                        } else {
+
+                        }
+
+
+                    } else {
+                        System.out.print("数据异常！");
+                    }
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
-             };
-         }.start();
+            }
+        }.start();
+
+//         new Thread(){
+//             public void run() {
+//                try {
+//                    Thread.sleep(3000);
+//                    //意图：激活Activity或者Service的重要对象。
+//                    startActivity(new Intent(getBaseContext(), HomeActivity.class));
+//                    //关闭
+//                    finish();
+//                } catch (InterruptedException e) {
+//                    // TODO Auto-generated catch block
+//                    e.printStackTrace();
+//                }
+//             };
+//         }.start();
     }
+    private int localVersionCode;
+    private static final int CODE_UPDATE = 200;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case CODE_UPDATE:
+                    //弹出对话框
+                    System.out.println("弹出对话框");
+
+                    break;
+            }
+        }
+    };
 }
---
+++ app/src/main/java/com/example/huwang/mobilesafe20/utils/HttpUtil.java
@@ -0,0 +1,51 @@
+package com.example.huwang.mobilesafe20.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Created by huwang on 2017/3/19.
+ */
+
+public class HttpUtil {
+    public static String get(String urlString) {
+        String result = null;
+        //创建链接
+        try {
+            URL url = new URL(urlString);
+            //打开链接
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            //设置get连接时长5000
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            if (200 == conn.getResponseCode()) {
+                InputStream input = conn.getInputStream();
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                //准备一个缓冲区
+                byte[] buffer = new byte[1024];
+                int len = 0;
+                while ((len = input.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+                out.flush();
+                byte[] bytes = out.toByteArray();
+                out.close();
+                result = new String(bytes);
+                //关闭流
+                input.close();
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
